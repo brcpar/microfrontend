9403b35348a99a71f8e4c24c246622044546f953
Bruce Parish <brcpar@gmail.com>
2019-11-07 15:54:00 -0500

    load vue and vuerouter in shell app and allow that dependency to be shared amongs all applications

--- navigation/vue.config.js
+++ navigation/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     chainWebpack: config => {
       config.devServer.set('inline', false)
       config.devServer.set('hot', true)
-      //config.externals(['vue', 'vue-router'])
+      config.externals(['vue', 'vue-router'])
     },
     filenameHashing: false,
   }
