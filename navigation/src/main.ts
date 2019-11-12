9d5c932ebfd9747f0cd93b1bdf0f748f5761c756
Bruce Parish <bruce.parish@cincpro.com>
2019-11-12 11:20:32 -0500

    Making navigation work with using external vue import while running it separately.

--- navigation/src/main.ts
+++ navigation/src/main.ts
@@ -1,12 +1,23 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import GlobalStorePlugin from './plugins/plugins';
+import singleSpaVue from 'single-spa-vue';
 import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  vuetify,
-  render: (h) => h(App)
-}).$mount('#app');
+const vueLifecycles = singleSpaVue({
+  Vue,
+  appOptions: {
+    render: (h: any) => h(App),
+    router,
+    GlobalStorePlugin,
+    vuetify,
+    el: '#app'
+  },
+});
+
+export const bootstrap = vueLifecycles.bootstrap;
+export const mount = vueLifecycles.mount;
+export const unmount = vueLifecycles.unmount;
